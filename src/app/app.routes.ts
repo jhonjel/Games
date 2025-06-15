a2b0c2169696718a03ed75f1740c0ab25a19b7f2
juanhts04 <juancarlossintor@gmail.com>
2025-06-15 15:51:17 -0500

    ww

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
 import { IniciarComponent } from './components/iniciar/iniciar.component';
 import { RegistrarComponent } from './components/registrar/registrar.component';
-import { BoardComponent } from './components/board/board.component';
 
 export const routes: Routes = [
   { path: '', component: IniciarComponent },
-  { path: 'iniciar', component: IniciarComponent },
   { path: 'registrar', component: RegistrarComponent },
   { path: 'juego', component: BoardComponent },
 ];
